73c805bd69deeb21de4137ffd88666fc2da8fa49
Didier Carrillo <didiercarrillo40@gmail.com>
2025-04-08 19:49:37 -0500

    added space after printing a single element in forloop

--- matrix.cpp
+++ matrix.cpp
@@ -125,7 +125,7 @@ void Matrix::print_matrix() const {
 
     for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
-            cout << data[i][j];
+            cout << data[i][j] << " ";
         }
         cout << "\n";
     }
