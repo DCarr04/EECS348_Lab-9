05dfb5326af9460b7a11ff84c9648aae6c26c265
Didier Carrillo <didiercarrillo40@gmail.com>
2025-04-09 22:23:29 -0500

    removed template T and went back to int

--- matrix.hpp
+++ matrix.hpp
@@ -6,24 +6,24 @@
 #include <string>
 
 // all of your function definitions should be in this file now
-template <typename T>
+
 class Matrix {
 private: 
     size_t size;
     
 public:
-    std::vector<std::vector<T> > data;
+    std::vector<std::vector<int> > data;
     Matrix(std::size_t N);
-    Matrix(std::vector<std::vector<T> > nums);
+    Matrix(std::vector<std::vector<int> > nums);
     ~Matrix();
 
     Matrix operator+(const Matrix &rhs) const;
     Matrix operator*(const Matrix &rhs) const;
-    void set_value(std::size_t i, std::size_t j, T n);
-    T get_value(std::size_t i, std::size_t j) const;
+    void set_value(std::size_t i, std::size_t j, int n);
+    int get_value(std::size_t i, std::size_t j) const;
     int get_size() const;
-    T sum_diagonal_major() const;
-    T sum_diagonal_minor() const;
+    int sum_diagonal_major() const;
+    int sum_diagonal_minor() const;
     void swap_rows(std::size_t r1, std::size_t r2);
     void swap_cols(std::size_t c1, std::size_t c2);
     void print_matrix() const;
